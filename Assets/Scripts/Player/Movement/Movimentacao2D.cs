ec2a7315af0b985bd7cb83387f0ff9f8a7ec6816
EduardoBoaventura <eduardo.boaveentura@yahoo.com.br>
2017-02-27 22:38:35 -0300

    Script 2D
    melhorei o script de movimentação 2D e fiz o personagem nao sair mais da
    tela

--- Assets/Scripts/Player/Movement/Movimentacao2D.cs
+++ Assets/Scripts/Player/Movement/Movimentacao2D.cs
@@ -13,36 +13,63 @@ public class Movimentacao2D : MonoBehaviour {
 	//Define o numero do player.
 	public int PlayerNumber;
 
+	public Camera CameraMain;
+
+	private Vector2 Movement;
+
+	private Rigidbody2D rigidbody2D;
 	void Start () {
-		
-	}
-	
-
-
-	void Update () {
-		Movement ();
+		rigidbody2D = gameObject.GetComponent<Rigidbody2D> ();
 	}
 
-	void Movement() {
-		//movimenta o Player pra cima
-		if(Input.GetAxisRaw("Vertical P" + PlayerNumber) > 0){
-			transform.Translate(Vector3.up*Time.deltaTime*Speed); 
-		}
-		//movimenta o Player pra baixo
-		if(Input.GetAxisRaw("Vertical P" + PlayerNumber) < 0){
-			transform.Translate(Vector3.down*Time.deltaTime*Speed); 
-		}
-		//movimenta o Player pra esquerda
-		if(Input.GetAxisRaw("Horizontal P" + PlayerNumber) < 0){
-			// vira a sprite pro lado esquerdo
-			Spriter.GetComponent<SpriteRenderer> ().flipX = true;
-			transform.Translate(Vector3.left*Time.deltaTime*Speed); 
-		}
-		//movimenta o Player pra direita
-		if(Input.GetAxisRaw("Horizontal P" + PlayerNumber) > 0){
-			// vira a sprite pro lado direito
+	void FixedUpdate () {
+		Movementation ();
+		TestPosition ();
+	}
+
+	void Movementation() {
+
+		//Define a direção no eixo Y
+		float InputY = Input.GetAxisRaw("Vertical P" + PlayerNumber);
+		//Define a direção no eixo X
+		float InputX = Input.GetAxisRaw ("Horizontal P" + PlayerNumber);
+
+		//Define pra qual lado o sprite vai estar virado
+		if(InputX > 0)
 			Spriter.GetComponent<SpriteRenderer> ().flipX = false;
-			transform.Translate(Vector3.right*Time.deltaTime*Speed); 
-		}
+		else if (InputX < 0)
+			Spriter.GetComponent<SpriteRenderer> ().flipX = true;
+
+		//Registra a direção que o personagem vai se mover
+		Movement = new Vector2 (InputX * Speed, InputY * Speed);
+
+		//Move o personagem na direção indicada.
+		rigidbody2D.velocity = Movement;
+
+	}
+
+	//testa a posição do personagem em relação a camera
+	void TestPosition(){
+		//Calcula a distancia entre o personagem e a camera.
+		var DistanceZ = (transform.position - CameraMain.transform.position).z;
+
+		//Calcula o ponto maximo de movimentação pra esquerda.
+		var Leftborder = CameraMain.ViewportToWorldPoint (new Vector3 (0.01f, 0, DistanceZ)).x;
+
+		//Calcula o ponto maximo de movimentação pra direita.
+		var Rightborder = CameraMain.ViewportToWorldPoint (new Vector3 (0.99f, 0, DistanceZ)).x;
+
+		//Calcula o ponto maximo de movimentação pra Baixo.
+		var Bottomborder = CameraMain.ViewportToWorldPoint (new Vector3 (0, -0.7f, DistanceZ)).y;
+
+		//Calcula o ponto maximo de movimentação pra Cima.
+		var Topborder = CameraMain.ViewportToWorldPoint (new Vector3 (0, 1.19f, DistanceZ)).y;
+
+		//Mantem o personagem sempre dentro do espaço da camera.
+		transform.position = new Vector3 (
+			Mathf.Clamp (transform.position.x, Leftborder, Rightborder),
+			Mathf.Clamp (transform.position.y, Bottomborder, Topborder), 
+			transform.position.z);
+
 	}
 }
---
+++ Assets/Scripts/Player/Movement/TestPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPosition : MonoBehaviour {
+
+	public Camera CameraMain;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var DistanceZ = (transform.position - CameraMain.transform.position).z;
+
+		var Leftborder = CameraMain.ViewportToWorldPoint (new Vector3 (0, 0, DistanceZ)).x;
+
+		var Rightborder = CameraMain.ViewportToWorldPoint (new Vector3 (1, 0, DistanceZ)).x;
+
+		var Topborder = CameraMain.ViewportToWorldPoint (new Vector3 (0, 0, DistanceZ)).y;
+
+		var Bottomborder = CameraMain.ViewportToWorldPoint (new Vector3 (0, 1.2f, DistanceZ)).y;
+
+		transform.position = new Vector3 (
+			Mathf.Clamp (transform.position.x, Leftborder, Rightborder),
+			Mathf.Clamp (transform.position.y, Topborder, Bottomborder), 
+			transform.position.z);
+		
+	}
+}
