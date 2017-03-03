0aeb1ebced8bbfbe099b102a0953bdfe5e1357d6
EduardoBoaventura <eduardo.boaveentura@yahoo.com.br>
2017-03-02 21:57:36 -0300

    Atualizações do script IA
    Atualizei o script da ia e comentei, tentando fazer ele ser menos
    "engessado"

--- Assets/Scripts/Enemy/EnemyIA.cs
+++ Assets/Scripts/Enemy/EnemyIA.cs
@@ -6,13 +6,18 @@ public class EnemyIA : MonoBehaviour {
 
 	#region Privates Variables
 	private Rigidbody RB;
+	//Direções que o inimigo pode escolher.
 	private bool Left,Front,Right,Back;
+	//tempo de ataque e troca de direção.
 	private float timer,CDAtk;
+	//Distancia que ele começa a rondar.
+	public float DistX;
 	#endregion
 
 	[Header("Ativa/Desativa o script")]
 	public bool Active;
 
+	//Estados do inimigo.
 	public enum State {Chase, Round, Attack};
 	[Header("Estado Atual")]
 	public State MyActualState = State.Chase;
@@ -20,16 +25,20 @@ public class EnemyIA : MonoBehaviour {
 	public Transform Target;
 
 	[Header("Tempo para trocar de direção")]
-	[Range(2,8)]
+	[Range(1,8)]
 	public float timerMax;
 
 	[Header("Tempo de recarga do Ataque")]
 	[Range(8,30)]
 	public float AttackCD;
 
-	[Header("Distancia minima de perseguição")]
-	[Range(10,20)]
+	[Header("Distancia minima para Ronda")]
+	[Range(5,20)]
 	public float MinDist;
+
+	[Header("Distancia Maxima para Ronta")]
+	[Range(10,30)]
+	public float MaxDist;
 
 	[Header("Distancia para ataque")]
 	[Range(2,4)]
@@ -49,7 +58,7 @@ public class EnemyIA : MonoBehaviour {
 
 	void Start () {
 		RB = gameObject.GetComponent<Rigidbody> ();
-		Front = true;
+		SelectSide ();
 		CDAtk = AttackCD;
 	}
 	
@@ -65,55 +74,73 @@ public class EnemyIA : MonoBehaviour {
 		}
 	}
 		
+
+	#region Estado de Chase
 	void ChaseState(){
+		//randomiza a distancia que ele vai começar a rondar.
+		DistX = Random.Range (MinDist, MaxDist);
+		//mantem o inimigo olhando pro player
 		var rotation = Quaternion.LookRotation (Target.position - transform.position);
 		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * RotateSpeed);
 
-		if (Vector3.Distance (transform.position, Target.position) > MinDist) {
-			RB.velocity = transform.forward * Speed;
-		} else {
+		//calcula se o inimigo esta dentro da distacia para ronda ou não.
+		if (Vector3.Distance (transform.position, Target.position) < DistX) {
 			MyActualState = State.Round;
+		} else {
+			RB.velocity = transform.forward * Speed;
 		}
-			
-
 	}
+	#endregion
+
+	#region Estado de Ronda
 	void RoundState(){
+		//mantem o inimigo olhando pro player
 		var rotation = Quaternion.LookRotation (Target.position - transform.position);
 		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * RotateSpeed);
+		//Tempo de ataque.
 		CDAtk -= Time.deltaTime;
-		if (Vector3.Distance (transform.position, Target.position) < MinDist && CDAtk > 0) {
+
+		//verifica a distancia do jogador.
+		if (Vector3.Distance (transform.position, Target.position) < DistX + 10 && CDAtk > 0) {
 			timer += Time.deltaTime;
 			if (timer > timerMax) {
+				//seleciona uma direção depois de certo tempo.
 				SelectSide ();
 				timer = 0;
 			}
+			//verifica se o jogador esta muito proximo e se afasta, se caso o tempo de ataque nao estiver zerado.
 			if (Vector3.Distance (transform.position, Target.position) < AtkDist && CDAtk > 0) {
-				RB.velocity = -transform.forward * RoundSpeed;
-				if (Front) {
-					Front = false;
-					SelectSide ();
-				}
-
-			} else {
-				if (Left) {
-					RB.velocity = -transform.right * RoundSpeed;
-				} else if (Right) {
-					RB.velocity = transform.right * RoundSpeed;
-				} else if (Front) {
-					RB.velocity = transform.forward * RoundSpeed;
-				} else if (Back) {
-					RB.velocity = -transform.forward * RoundSpeed;
-				}
+				Front = false;
+				Back = true;
 			}
+			#region Movimentação nas direções.
+			if (Left && Front) {
+				RB.velocity = (-transform.right + transform.forward) * Random.Range(0,RoundSpeed);
+			} else if (Right && Front) {
+				RB.velocity = (transform.right + transform.forward) * Random.Range(0,RoundSpeed);
+			} else if (Left && Back) {
+				RB.velocity = (-transform.right + (-transform.forward)) * Random.Range(0,RoundSpeed);
+			} else if (Right && Back) {
+				RB.velocity = (transform.right + (-transform.forward)) * Random.Range(0,RoundSpeed);
+			} else if (Right) {
+				RB.velocity = transform.right * Random.Range(0,RoundSpeed);
+			} else if (Left) {
+				RB.velocity = -transform.right * Random.Range(0,RoundSpeed);
+			}
+			#endregion
+	
 		}else if (Vector3.Distance (transform.position, Target.position) > AtkDist && CDAtk <= 0) {
 			RB.velocity = transform.forward * Speed;
-		} else if (Vector3.Distance (transform.position, Target.position) > MinDist)
+		}else if (Vector3.Distance (transform.position, Target.position) > DistX + 10)
 			MyActualState = State.Chase;
 
 		if (Vector3.Distance (transform.position, Target.position) < AtkDist && CDAtk <= 0) {
 			MyActualState = State.Attack;
 		}
 	}
+	#endregion
+	#region Estado de Ataque.
+	//Decidir o ataque.
 	void AttackState(){
 		StartCoroutine (Attack());
 
@@ -127,31 +154,47 @@ public class EnemyIA : MonoBehaviour {
 		else
 			MyActualState = State.Round;
 	}
+	#endregion
+
+	#region Seleção de direção
+	//Seleciona Randomicamente uma direção para se mover.
 	void SelectSide(){
-		float R = Random.Range (0, 8);
-		if (R <= 2) {
+		float R = Random.Range (0, 12);
+		if (R <= 2 && Vector3.Distance (transform.position, Target.position) > AtkDist) {
+			Left = true;
+			Right = false;
+			Front = true;
+			Back = false;
+		} else if (R <= 4 && Vector3.Distance (transform.position, Target.position) > AtkDist) {
+			Left = false;
+			Right = true;
+			Front = true;
+			Back = false;
+		} else if (R <= 6 && Vector3.Distance (transform.position, Target.position) < MinDist) {
 			Left = true;
 			Right = false;
 			Front = false;
-			Back = false;
-		} else if (R <= 4) {
+			Back = true;
+		} else if (R <= 8 && Vector3.Distance (transform.position, Target.position) < MinDist) {
+			Left = false;
+			Right = true;
+			Front = false;
+			Back = true;
+		} else if (R <= 6) {
 			Left = false;
 			Right = true;
 			Front = false;
 			Back = false;
-		} else if (R <= 6 && Vector3.Distance (transform.position, Target.position) > AtkDist) {
-			Left = false;
-			Right = false;
-			Front = true;
-			Back = false;
-		} else if(R <= 8 && Vector3.Distance (transform.position, Target.position) < MinDist) {
-			Left = false;
+		} else if (R <= 12) {
+			Left = true;
 			Right = false;
 			Front = false;
 			Back = true;
+		} else {
+			SelectSide ();
 		}
 	}
-
+	#endregion
 	//Testes Pra seleção de Foco
 	/* Transform SelectTarget (Transform P1, Transform P2,Transform P3){
 		//Distancia Do Player1 em X e em Z
