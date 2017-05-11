1dfff65105b7cdfa876eac0ec40ecab68c8a5e21
Wauei <waueigt@gmail.com>
2017-05-11 01:08:33 -0300

    Tentativa ir pra tela
    Tentei fazer o mosquito ir para a tela mas nao funcionou direito.

--- Assets/FSM/FSMMosquito.cs
+++ Assets/FSM/FSMMosquito.cs
@@ -6,7 +6,7 @@ public class FSMMosquito : MonoBehaviour
 {
 
     #region FSM States
-    public enum FSMStates { Idle, Walk, ATK1, ATK2, Damage, StepBack, Grappled, Thrown, DrainLife, Die, Fall, Transition, Patrol };
+    public enum FSMStates { Idle, Walk, ATK1, ATK2, Damage, StepBack, Grappled, Thrown, DrainLife, Die, Fall, Transition, Patrol, GoToScreen };
     public FSMStates state = FSMStates.Idle;
     #endregion
 
@@ -24,6 +24,10 @@ public class FSMMosquito : MonoBehaviour
     public float SafeDist = 10f;        //Area Para o Npc desistir de perceguir o Player
     public float EnemyDist = 2f;        //Area para Iniciar o Ataque
     public float Life = 100;              //Vida Do NPC
+
+    public Transform camScreen;
+    float _2dY, _2dX;
+    public bool onScreen;
 
 
     public bool TakeDamage = false;      //Verifica se o player levou dano
@@ -46,6 +50,14 @@ public class FSMMosquito : MonoBehaviour
 	[SerializeField] private float MaxLife;
 	private bool returned;
 	[SerializeField] private Collider DeathCollider;
+    [SerializeField] private bool reachScreen;
+    public float velTransicao;
+    public Transform model;
+    private bool Jumping;
+    public Transform direcoes;
+    public bool toWorld;
+    public CameraControl DollyCam;
+    private bool descer = false;
 
 
 
@@ -150,6 +162,10 @@ public class FSMMosquito : MonoBehaviour
                 Transition();
                 break;
 
+            case FSMStates.GoToScreen:
+                GoToScreen();
+                break;
+
 
         }
 
@@ -213,6 +229,61 @@ public class FSMMosquito : MonoBehaviour
     public void HitBoxOff()
     {
 		hitbox.GetComponent<Collider> ().enabled = false;
+    }
+
+    void goToScreen()
+    {
+        //testa se o jogador pediu pra ir pra tela
+        if (onScreen)
+        {
+            //variavel para parar de controlar o jogador após ele chegar na tela
+            if (!reachScreen)
+            {
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, new Vector3(camScreen.localPosition.x + _2dX,
+                    camScreen.localPosition.y + _2dY, camScreen.localPosition.z), velTransicao);
+                //rotação pra deixar o modelo pronto pra movimentação na tela e colocar os pés do modelo no "vidro"
+                transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.AngleAxis(90, Vector3.right), velTransicao * 10);
+                transform.localScale = Vector3.MoveTowards(transform.localScale, new Vector3(0.5f, 0.5f, 0.5f), velTransicao / 10);
+                if (transform.localPosition == new Vector3(camScreen.localPosition.x + _2dX, camScreen.localPosition.y + _2dY, camScreen.localPosition.z) && transform.localRotation == Quaternion.AngleAxis(90, Vector3.right) && transform.localScale == new Vector3(0.25f, 0.25f, 0.25f))
+                {
+                    Jumping = false;
+                    reachScreen = true;
+                }
+            }
+
+            //se o jogador pedir pra descer
+            if (descer)
+            {
+                //tira ele do parent, ativa a variavel que fala que é pra ir pro 3D, liga a gravidade, e desativa a variavel que fala q ele ta na tela
+                //SetAnimOffScreen();
+                transform.SetParent(null);
+                toWorld = true;
+                reachScreen = false;
+                onScreen = false;
+            }
+        }
+    }
+
+    void exitScreen()
+    {
+        //se a variavel q fala pra ele sair da tela tiver ligada ele deve consertar o tamanho e a rotação do personagem
+        if (toWorld)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(DollyCam.target.position.x,
+                    1f, DollyCam.target.position.z), velTransicao);
+            transform.localScale = Vector3.MoveTowards(transform.localScale, new Vector3(1, 1, 1), velTransicao / 10);
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.AngleAxis(0, Vector3.right), velTransicao * 5);
+            model.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(0, 0, 0), velTransicao * 5);
+            direcoes.transform.localRotation = Quaternion.AngleAxis(0, Vector3.right);
+        }
+        //uma vez que o tamanho esta ok a variavel pode ficar falsa.
+        if (transform.localScale == new Vector3(1, 1, 1) && transform.localRotation.x == 0f)
+        {
+            rb.useGravity = true;
+            model.localRotation = Quaternion.Euler(0, 0, 0);
+            toWorld = false;
+        }
+
     }
 
     #endregion
@@ -424,9 +495,29 @@ public class FSMMosquito : MonoBehaviour
     }
     #endregion
 
+    #region GoToScreen
+    private void GoToScreen()
+    {
+        rb.useGravity = false;
+        transform.SetParent(camScreen);
+        _2dX = Random.Range(-1.5f, +1.5f);
+        _2dY = Random.Range(-0.8f, +0.8f);
+        onScreen = true;
+
+        onScreen = true;
+        goToScreen();
+    }
+    #endregion
+
+
     #region Transition
     private void Transition()
     {
+
+        
+
+
+
 
     }
     #endregion
