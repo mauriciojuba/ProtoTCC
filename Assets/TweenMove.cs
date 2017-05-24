d60b52e038f5e6ec67a70f6f1a61b5cce98a3e79
Wauei <waueigt@gmail.com>
2017-05-24 00:35:26 -0300

    Camera passado pelos buracos na parede (so ida)

---
+++ Assets/TweenMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenMove : MonoBehaviour
+{
+
+    public bool move;
+    public string Path;
+    public float time;
+
+    public GameObject target;
+    
+    // public GameObject Target;
+
+    public bool volta;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // transform.LookAt(Target.transform.position);
+
+        if (move)
+        {
+            iTween.MoveTo(gameObject,iTween.Hash("path", iTweenPath.GetPath(Path), "time", time, "orienttopath", true));
+
+            move = false;
+        }
+
+        if (volta)
+        {
+            iTween.MoveFrom(gameObject, iTween.Hash("path", iTweenPath.GetPath(Path), "time", time));
+
+            volta = false;
+        }
+    }
+
+}
---
+++ Assets/tweenSelectMenu.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tweenSelectMenu : MonoBehaviour {
+
+    public enum FSMStates { Options, Game, Credits, Exit ,Menu};
+    public FSMStates state = FSMStates.Options;
+
+
+    public string Select;
+
+    public void FixedUpdate()
+    {
+        switch (state)
+        {
+
+            case FSMStates.Options:
+                Options();
+                break;
+
+            case FSMStates.Game:
+                Game();
+                break;
+
+            case FSMStates.Credits:
+                Credits();
+                break;
+
+            case FSMStates.Exit:
+                Exit();
+                break;
+
+            case FSMStates.Menu:
+                Menu();
+                break;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void Menu()
+
+    {
+
+        // if (........)
+        //state = FSMStates.Estado_2;
+    }
+
+    private void Options()
+    {
+
+        // if (........)
+        //state = FSMStates.Estado_2;
+    }
+    private void Game()
+
+    {
+
+        // if (........)
+        //state = FSMStates.Estado_2;
+    }
+    private void Credits()
+
+    {
+
+        // if (........)
+        //state = FSMStates.Estado_2;
+    }
+    private void Exit()
+
+    {
+
+        // if (........)
+        //state = FSMStates.Estado_2;
+    }
+
+}
