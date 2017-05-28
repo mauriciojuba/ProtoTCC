59d621147f1a830a2d241fe09275ef724ee46ca9
Wauei <waueigt@gmail.com>
2017-05-28 20:30:16 -0300

    Teste SFX

--- Assets/Scripts/AudioManager/SFX.cs
+++ Assets/Scripts/AudioManager/SFX.cs
@@ -5,6 +5,8 @@ using UnityEngine.Audio;
 
 public class SFX : MonoBehaviour {
 
+
+    public string Objeto;
     public AudioMixerGroup Mixer;
 	[SerializeField] private AudioSource Audio;
 
@@ -14,4 +16,27 @@ public class SFX : MonoBehaviour {
 		Audio = GetComponent<AudioSource> ();
 		Audio.outputAudioMixerGroup = Mixer;
     }
+
+    public void AplicaMixer()
+    {
+        Audio = GetComponent<AudioSource>();
+        Audio.outputAudioMixerGroup = Mixer;
+    }
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if(hit.gameObject.name == "Chao")
+        {
+            if(Objeto == "Vidro")
+            {
+                SoundManager.PlaySFX(gameObject,SoundManager.LoadFromGroup("Objetos quebrando vidro"));
+                Audio = GetComponent<AudioSource>();
+                Audio.outputAudioMixerGroup = Mixer;
+            }
+
+
+
+        }
+    }
 }
+
