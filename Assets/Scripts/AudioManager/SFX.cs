71035ae3a7ea3a8c9540c6ba01b1efa01fb063a3
Wauei <waueigt@gmail.com>
2017-05-28 03:04:32 -0300

    Mixer teste

--- Assets/Scripts/AudioManager/SFX.cs
+++ Assets/Scripts/AudioManager/SFX.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SFX : MonoBehaviour {
+
+    public AudioMixerGroup Mixer;
 
     public void PlaySoundSFX(string Name)
     {
         SoundManager.PlaySFX(Name);
+        SoundManager.GetCurrentAudioSource().outputAudioMixerGroup = Mixer;
     }
 }
