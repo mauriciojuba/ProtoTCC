68ab763d380223ed9db0a59baac3ae160f72224e
Wauei <waueigt@gmail.com>
2017-05-28 02:37:44 -0300

    TESTE sound manager

---
+++ Assets/Scripts/AudioManager/SFX.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX : MonoBehaviour {
+
+    public void PlaySoundSFX(string Name)
+    {
+        SoundManager.PlaySFX(Name);
+    }
+}
