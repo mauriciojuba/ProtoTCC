772d763ed96d35c09ba366505877c04aff962f6b
mauriciojuba <mauriciojuba@outlook.com>
2017-05-29 20:13:08 -0300

    Options Montagem e Jogo de Camera

---
+++ Assets/Scripts/Menus/Menus/Options/ControlCamerasOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCamerasOptions : MonoBehaviour {
+
+	public GameObject ligaCamera, desligaCamera;
+
+	void OnTriggerEnter(Collider hit){
+		if(hit.CompareTag("Player1_3D")){
+			ligaCamera.SetActive(true);
+			desligaCamera.SetActive(false);
+		}
+
+	}
+}
