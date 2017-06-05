ddfe241154f683b0a420e298ce3f58dc44e7e8c1
victortomioka <vtomioka@gmail.com>
2017-06-05 01:42:26 -0300

    drenagem de vida

---
+++ Assets/Resources/Effects/Scripts/ParticleHoming.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleHoming : MonoBehaviour {
+
+	ParticleSystem emitter;
+	public Transform target;
+	ParticleSystem.Particle[] particles;
+	float speed;
+	public float multiplier = 1.0f;
+
+
+	void Start () {
+		emitter = gameObject.GetComponent<ParticleSystem>();
+		speed = emitter.main.startSpeed.constant;
+		if (particles == null || particles.Length < emitter.maxParticles) {
+			particles = new ParticleSystem.Particle[emitter.maxParticles];
+		}
+	}
+	
+
+	void LateUpdate () {
+		int particlesAlive = emitter.GetParticles (particles);
+
+		for(int i = 0; i < particlesAlive ; i++)
+		{
+			particles[i].position = Vector3.Slerp (particles[i].position, target.position, Time.deltaTime*speed*multiplier);
+		}
+
+		emitter.SetParticles(particles,particlesAlive);
+	}
+}
