92215eb84f0ed7ca477275355af206421c89df0e
EduardoBoaventura <eduardo.boaveentura@yahoo.com.br>
2017-06-06 17:40:55 -0300

    Caixa dropando item e causando dano

--- Assets/Scripts/Objects/DestruirObjeto.cs
+++ Assets/Scripts/Objects/DestruirObjeto.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestruirObjeto : MonoBehaviour {
 
 	public bool Throwed;
+	[SerializeField] private float Damage = 50;
 
 	void OnCollisionEnter(Collision hit){
 		if (hit.gameObject.CompareTag ("Roomba")) {
@@ -15,6 +16,13 @@ public class DestruirObjeto : MonoBehaviour {
 			if (this.GetComponent<Life> () != null) {
 				this.GetComponent<Life> ().LifeQuant = 0;
 			}
+			if (hit.gameObject.tag != "Player1_3D" && hit.gameObject.tag != "Player2_3D") {
+				if (hit.gameObject.GetComponent<Life> () != null) {
+					hit.gameObject.GetComponent<Life> ().LifeQuant -= Damage;
+					hit.gameObject.GetComponent<Life> ().UpdateLife ();
+				}
+			}
+		
 		}
 	}
 }
