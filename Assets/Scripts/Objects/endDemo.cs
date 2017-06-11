3d5fccdaa1d4358c0cca4e2f9e0dd3746b21464f
mauriciojuba <mauriciojuba@outlook.com>
2017-06-11 20:57:22 -0300

    endDemo

--- Assets/Scripts/Objects/endDemo.cs
+++ Assets/Scripts/Objects/endDemo.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class endDemo : MonoBehaviour {
 
 	void OnTriggerEnter(Collider hit){
-		if(hit.CompareTag("Player1_3D")){
+		if(hit.CompareTag("Player1_3D") || hit.CompareTag("Player2_3D")){
 			UnityEngine.SceneManagement.SceneManager.LoadScene ("Thanks");
 		}
 	}
