65ee7bf1c59d1ed687d631573066fdeb47005365
Wauei <waueigt@gmail.com>
2017-07-19 19:08:13 -0300

    Pequenos ajustes

--- Assets/Colect.cs
+++ Assets/Colect.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Candy : MonoBehaviour {
+public class Colect : MonoBehaviour {
 
 	bool Reduct;
 	[SerializeField] private float speedReduct;
